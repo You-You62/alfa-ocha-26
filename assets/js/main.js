624dfac88308f956eb0072c3e445ab71cb5abf65
You-You62 <89888546+You-You62@users.noreply.github.com>
2021-09-15 10:04:40 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'AAA',
+          q1: '3624',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '受付',
            q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '5',
           // q2: 'かかか',
           // q3: 'ききき',
         }
